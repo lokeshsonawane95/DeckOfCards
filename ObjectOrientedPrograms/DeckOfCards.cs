39117abdc27f477ffddef12168f3734c8ca655cf
lokeshsonawane95 <lokeshsonawane95@gmail.com>
2022-05-21 21:27:49 +0530

    Added to main : Deck of cards program

---
+++ ObjectOrientedPrograms/DeckOfCards.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectOrientedPrograms
+{
+    internal class DeckOfCards
+    {
+        Random random = new Random();
+        public int suitIndex, rankIndex;
+        List<string> cards = new List<string>();
+        string[] suit = { "Clubs", "Diamonds", "Hearts", "Spades" };
+        string[] rank = { "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace" };
+        List<string>[,] player;
+        public DeckOfCards()
+        {
+            player = new List<string>[2, 2];
+
+            //Inititalizing the array of Lists
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    player[i, j] = new List<string>();
+                }
+            }
+        }
+
+        //Shuffle logic
+        public void Shuffle()
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    for (int k = 0; k < 2; k++)
+                    {
+                        ExecuteAgain:
+                        suitIndex = random.Next(suit.Length);
+                        rankIndex = random.Next(rank.Length);
+                        string shuffledCard = rank[rankIndex] + " of " + suit[suitIndex];
+                        if (cards.Contains(shuffledCard))
+                        {
+                            goto ExecuteAgain;
+                        }
+                        else
+                        {
+                            cards.Add(shuffledCard);
+                            player[j, k].Add(shuffledCard);
+                        }
+                    }
+                }
+            }
+        }
+
+        //Displaying cards of each player
+        public void Display()
+        {
+            int count = 1;
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    Console.WriteLine("Player " + count + " has these cards");
+                    //Console.WriteLine("Player " + player[i, j]);
+
+                    //Print the cards of each player at index [i, j]
+                    foreach (string card in player[i, j])
+                    {
+                        Console.WriteLine(card);
+                    }
+                    count++;
+                }
+            }
+        }
+    }
+}
---
+++ ObjectOrientedPrograms/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ObjectOrientedPrograms
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            DeckOfCards deckOfCards = new DeckOfCards();
+            deckOfCards.Shuffle();
+            deckOfCards.Display();
+        }
+    }
+}
